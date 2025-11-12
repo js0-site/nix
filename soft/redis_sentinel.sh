73213dba49ec8eb569a645a96cf8b989109d9286
js0-site <jssite@googlegroups.com>
2025-11-12 21:26:43 +0800

    fix(redis_sentinel): update sentinel config to support multiple clusters and known slaves / 更新哨兵配置以支持多集群和已知从节点
    - replace sentinel.sh with ip_li.env (soft/redis_sentinel.sh)
    - add support for multiple clusters by configuring known slaves (soft/redis_sentinel.sh)
    
    - 将 sentinel.sh 替换为 ip_li.env（soft/redis_sentinel.sh）
    - 添加对多个集群的支持，通过配置已知从节点（soft/redis_sentinel.sh）

--- soft/redis_sentinel.sh
+++ soft/redis_sentinel.sh
@@ -5,7 +5,7 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 set -a
 cd $(dirname $DIR)/nix/vps/disk/etc/kvrocks
 . conf.sh
-. sentinel.sh
+. ip_li.env
 set +a
 set -x
 
@@ -32,11 +32,18 @@ rconf '^port .*' "port $R_SENTINEL_PORT"
 rconf '^dir .*' "dir $LOGDIR"
 rconf '^logfile .*' "logfile $LOGDIR/$NAME.log"
 
+# 这个是监控的集群的名称，其实可以监控多个集群
+rconf "s/mymaster/$R_SENTINEL_NAME/g"
+
 # 在Redis哨兵的配置文件中，sentinel monitor命令的最后一个参数是quorum，它表示需要多少个哨兵节点同意才能执行故障转移。如果你有3个哨兵节点，那么你应该将quorum设置为2。这是因为在一个3节点的哨兵集群中，只有当至少2个哨兵节点同意主节点已经掉线时，才会触发故障转移。这样设置可以确保在故障转移过程中，即使有一个哨兵节点出现故障或者网络分区，也不会误判主节点的状态，从而避免不必要的故障转移。
 
-rconf '^sentinel monitor mymaster .*' "sentinel monitor $R_SENTINEL_NAME $R_MASTER_IP $R_PORT 2"
+sed -i '/^sentinel monitor/d' "$NAME.conf"
+
+for ip in $KVROCKS_IP_LI; do
+  echo "sentinel known-slave $R_SENTINEL_NAME $ip $R_PORT" >>$NAME.conf
+done
+
 rconf '^protected-mode .*' 'protected-mode no'
-rconf "s/mymaster/$R_SENTINEL_NAME/g"
 
 send $NAME.conf /etc
 
